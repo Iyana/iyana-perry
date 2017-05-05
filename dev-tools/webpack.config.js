29780f7500a34441bc390750202caf12b4c79f1f
fgyara <freddi@iyana.co.uk>
2017-05-05 20:40:22 +0100

    Search for elements tagged with data-perry-template and call a function

--- dev-tools/webpack.config.js
+++ dev-tools/webpack.config.js
@@ -1,8 +1,18 @@
+var path = require('path');
+
+
+
 module.exports = {
-    entry: "../js/Perry.js",
+    entry: __dirname + "/../web_modules/Perry.js",
     output: {
         path: __dirname + "/../web/js/",
         filename: "Perry.js"
     },
-    devtool: "source-map"
+    devtool: "source-map",
+    resolve: {
+        modules: [
+            __dirname + "/../web_modules",
+            __dirname + "/../node_modules"
+        ]
+    }
 };
---
+++ web_modules/PerryGlobals.js
@@ -0,0 +1,5 @@
+export default {
+    tags: {
+        perryTemplateAttribute: "data-perry-template"
+    }
+};
